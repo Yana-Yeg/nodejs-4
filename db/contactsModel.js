386f1dd39e9514171d656e68347817497ddeeccd
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-05-18 10:17:49 +0200

    add mongoosePaginate

--- db/contactsModel.js
+++ db/contactsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
 const contactSchema = new mongoose.Schema({
   name: {
@@ -20,7 +21,7 @@ const contactSchema = new mongoose.Schema({
     ref: "user",
   },
 });
-
+contactSchema.plugin(mongoosePaginate);
 const Contacts = mongoose.model("contact", contactSchema);
 
 module.exports = {
